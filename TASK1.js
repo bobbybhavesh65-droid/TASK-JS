1e8860d5a815ae8f1e8606b70e5d047611a1fd04
bobbybhavesh65-droid <bobbybhavesh65@gmail.com>
2026-06-09 11:22:47 +0530

    Add files via upload

---
+++ TASK1.js
@@ -0,0 +1,115 @@
+// ==========================
+// Task 1: Variable Practice
+// ==========================
+
+let studentName = "Bhavesh";
+let studentAge = 24;
+let studentCourse = "MERN Stack";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+
+// ==========================
+// Task 2: Var, Let, Const
+// ==========================
+
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+company = "Google";
+salary = 30000;
+
+console.log(company);
+console.log(salary);
+console.log(country);
+
+
+// ==========================
+// Task 3: User Name
+// ==========================
+
+let userName = prompt("What is your name?");
+
+console.log("Welcome " + userName);
+
+
+// ==========================
+// Task 4: Confirm Box
+// ==========================
+
+let result = confirm("Do you know JavaScript?");
+
+console.log(result);
+
+
+// ==========================
+// Task 5: Student Information
+// ==========================
+
+let studentName1 = prompt("Enter Student Name");
+let studentAge1 = prompt("Enter Student Age");
+let studentCity = prompt("Enter Student City");
+
+console.log("Name : " + studentName1);
+console.log("Age : " + studentAge1);
+console.log("City : " + studentCity);
+
+
+// ==========================
+// Task 6: Alert Message
+// ==========================
+
+let course = "JavaScript";
+
+alert("Welcome to " + course);
+
+
+// ==========================
+// Task 7: Document Write
+// ==========================
+
+document.writeln("Hello Student");
+document.writeln("<br>");
+document.writeln("Welcome to JavaScript");
+document.writeln("<hr>");
+
+
+// ==========================
+// Task 8: Favorite Things
+// ==========================
+
+let favoriteFood = " Biryani";
+let favoriteColor = "Black";
+let favoriteMovie = "mos";
+
+console.log("Food : " + favoriteFood);
+console.log("Color : " + favoriteColor);
+console.log("Movie : " + favoriteMovie);
+
+
+// ==========================
+// Task 9: Mini Bio Data
+// ==========================
+
+let name = "Bhavesh";
+let age = 24;
+let phone = "8147505007";
+let email = "bhavesh@gmail.com";
+
+console.log("Name : " + name);
+console.log("Age : " + age);
+console.log("Phone : " + phone);
+console.log("Email : " + email);
+
+
+// ==========================
+// Task 10: Challenge
+// ==========================
+
+let userName2 = prompt("Enter your Name");
+let userCourse = prompt("Enter your Course");
+
+alert("Hello " + userName2 + ", Welcome to " + userCourse + " Course");
